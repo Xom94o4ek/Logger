e8cbb16ba71619746ce5efe0f858885427b66804
Xom94o4ek <irek199707@gmail.com>
2021-03-20 21:11:43 +0300

    Добавьте файлы проекта.

---
+++ Logger/Logger.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Logger
+{
+    public interface ILog
+    {
+        /// <summary>
+        /// Критичная ошибка:приложение не может далее функционировать
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        void Fatal(string message);
+
+        /// <summary>
+        /// Критичная ошибка:приложение не может далее функционировать
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="e">Exception</param>
+        void Fatal(string message, Exception e);
+
+        /// <summary>
+        /// Ошибка в работе приложения: операция расчета завершается, но приложение продолжает работу
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        void Error(string message);
+
+        /// <summary>
+        /// Ошибка в работе приложения: операция расчета завершается, но приложение продолжает работу
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="e">Exception</param>
+        void Error(string message, Exception e);
+
+        /// <summary>
+        /// Ошибка в работе приложения: операция расчета завершается, но приложение продолжает работу
+        /// </summary>
+        /// <param name="ex"></param>
+        void Error(Exception ex);
+
+        /// <summary>
+        /// Запись уникальных ошибок
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="e"></param>
+        void ErrorUnique(string message, Exception e);
+
+        /// <summary>
+        /// Предупреждение: на работу приложения не влияет, 
+        /// но может сообщать о потенциальных проблемах в расчете
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        void Warning(string message);
+
+        /// <summary>
+        /// Предупреждение: на работу приложения не влияет, 
+        /// но может сообщать о потенциальных проблемах в расчете
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="e">Exception</param>
+        void Warning(string message, Exception e);
+
+
+        /// <summary>
+        /// Пишет в лог уникальные в течении дня ошибки 
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <remarks>
+        /// Если в течении дня поступают сообщения с одинаковым содержанием,
+        ///  то в лог попадут только первые вхождения. 
+        /// По прошествию дня уникальность возобновляется.
+        /// </remarks>>
+        void WarningUnique(string message);
+
+        /// <summary>
+        /// Информирование: не влияет на работу приложения,
+        /// является инструментом информирования
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        void Info(string message);
+
+        /// <summary>
+        /// Информирование: не влияет на работу приложения,
+        /// является инструментом информирования
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        ///  /// <param name="e">Exception</param>
+        void Info(string message, Exception e);
+
+        /// <summary>
+        /// Информирование: не влияет на работу приложения,
+        /// является инструментом информирования
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="args">аргументы</param>
+        void Info(string message, params object[] args);
+
+        /// <summary>
+        /// Дебагирование: инструмент для трассировки и отладки
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        void Debug(string message);
+
+        /// <summary>
+        /// Дебагирование: инструмент для трассировки и отладки
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="e">Exception</param>
+        void Debug(string message, Exception e);
+
+        /// <summary>
+        /// Дебагирование: инструмент для трассировки и отладки
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args">аргументы</param>
+        void DebugFormat(string message, params object[] args);
+
+        /// <summary>
+        /// Запись системных логов информационного характера
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="properties"></param>
+        void SystemInfo(string message, Dictionary<object, object> properties = null);
+    }
+    class Logger : ILog
+    {
+        public DateTime now;
+        Dictionary<int, string> TodayErrors = new Dictionary<int, string>();
+        int ErrorCounter = 0;
+        Dictionary<int, string> TodayWarnings = new Dictionary<int, string>();
+        int WarningCounter = 0;
+        string FatalFile = "Fatal.txt";
+        string ErrorFile = "Error.txt";
+        string ErrorUniqFile = "UError.txt";
+        string WarningFile = "Warning.txt";
+        string WarningUniqFile = "UWarning.txt";
+        string InfoFile = "Info.txt";
+        string DebugFile = "Debug.txt";
+        string SystemInfoFile = "SInfo.txt";
+        public void Fatal(string message)
+        {
+            WriteToFile(FatalFile, TextFormating("FATAL", message));
+        }
+        public void Fatal(string message, Exception e)
+        {
+            WriteToFile(FatalFile, TextFormating("FATAL", message,e));
+        }
+        public void Error(string message)
+        {
+            ErrorCounter++;
+            WriteToFile(ErrorFile, TextFormating("ERROR", message));
+        }
+        public void Error(string message, Exception e)
+        {
+            ErrorCounter++;
+            TodayErrors.Add(ErrorCounter, $"{e.Message} - {e.StackTrace}");
+            WriteToFile(ErrorFile, TextFormating("ERROR", message, e));
+        }
+        public void Error(Exception ex)
+        {
+            ErrorCounter++;
+            TodayErrors.Add(ErrorCounter, $"{ex.Message} - {ex.StackTrace}");
+            WriteToFile(ErrorFile, TextFormating("ERROR", ex));
+        }
+        public void ErrorUnique(string message, Exception e)
+        {
+            if(!TodayErrors.ContainsValue($"{e.Message} - {e.StackTrace}"))
+            {
+                //Error(message, e);
+                //ErrorCounter++;
+                //TodayErrors.Add(TodayErrors.Count, $"{e.Message} - {e.StackTrace}");
+                WriteToFile(ErrorUniqFile, TextFormating("UERROR", message, e));
+            }
+            Error(message,e);
+        }
+        public void Warning(string message)
+        {
+            WarningCounter++;
+            TodayWarnings.Add(WarningCounter, message);
+            WriteToFile(WarningFile, TextFormating("WARNING", message));
+        }
+        public void Warning(string message, Exception e)
+        {
+            WarningCounter++;
+            TodayWarnings.Add(WarningCounter, message);
+            WriteToFile(WarningFile, TextFormating("WARNING", message, e));
+        }
+        public void WarningUnique(string message)
+        {
+            if (!TodayWarnings.ContainsValue(message))
+            {
+                //Error(message, e);
+                //WarningCounter++;
+                //TodayWarnings.Add(WarningCounter, message);
+                WriteToFile(WarningUniqFile, TextFormating("UWARNING", message));
+            }
+            Warning(message);
+        }
+        public void Info(string message)
+        {
+            WriteToFile(InfoFile, TextFormating("INFO", message));
+        }
+        public void Info(string message, Exception e)
+        {
+            WriteToFile(InfoFile, TextFormating("INFO", message, e));
+        }
+        public void Info(string message, params object[] args)
+        {
+            WriteToFile(InfoFile, TextFormating("INFO", message, args));
+        }
+        public void Debug(string message)
+        {
+            WriteToFile(DebugFile, TextFormating("DEBUG", message));
+        }
+        public void Debug(string message, Exception e)
+        {
+            WriteToFile(DebugFile, TextFormating("DEBUG", message, e));
+        }
+        public void DebugFormat(string message, params object[] args)
+        {
+            WriteToFile(DebugFile, TextFormating("DEBUG", message, args));
+        }
+        public void SystemInfo(string message, Dictionary<object, object> properties = null)
+        {
+            if (properties != null)
+            {
+                WriteToFile(SystemInfoFile, TextFormating("SYSINFO", message, properties));
+            }
+            else { WriteToFile(SystemInfoFile, TextFormating("SYSINFO", message)); }
+        }
+        public void WriteToFile(string FileName, string message)
+        {
+            now = DateTime.Now;
+            string path = @String.Format("Log/{0}", now.ToString("yyyy-MM-dd"));
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists) { dirInfo.Create(); TodayErrors.Clear(); ErrorCounter = 0; TodayWarnings.Clear(); WarningCounter = 0; }
+            File.AppendAllText(@String.Format("{0}/{1}", path, FileName), $"{now.ToString("dd.MM.yyyy")} {now.ToString("HH:mm:ss")} {message}\n");
+        }
+        public string TextFormating(string nametype, string message)
+        {
+            return $"({nametype}): {message}";
+        }
+        public string TextFormating(string nametype, string message, Exception e)
+        {
+            string ExcInfo = $"\n\t\t\t\tИсключение: {e.Message}\n\t\t\t\tМетод: {e.TargetSite}\n\t\t\t\tТрассировка стека: {e.StackTrace}";
+            return $"({nametype}): {message}{ExcInfo}";
+        }
+        public string TextFormating(string nametype, Exception e)
+        {
+            string ExcInfo = $"Исключение: {e.Message}\n\t\t\t\tМетод: {e.TargetSite}\n\t\t\t\tТрассировка стека: {e.StackTrace}";
+            return $"({nametype}): {ExcInfo}";
+        }
+        public string TextFormating(string nametype, string message, params object[] args)
+        {
+            string AllArgs = "";
+            for (int i = 0; i < args.Length; i++)
+            {
+                AllArgs += "\n\t\t\t\t" + args[i];
+            }
+            return $"({nametype}): {message}{AllArgs}";
+        }
+        public string TextFormating(string nametype, string message, Dictionary<object, object> properties)
+        {
+            string AllArgs = "";
+            foreach (KeyValuePair<object, object> keyValue in properties)
+            {
+                AllArgs += $"\n\t\t\t\t{keyValue.Key} - {keyValue.Value}";
+            }
+            return $"({nametype}): {message}{AllArgs}";
+        }
+    }
+}
---
+++ Logger/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger Log = new Logger();
+            int toExit = 1;
+            while (toExit != 0)
+            {
+                Console.WriteLine("Введите делитель:");
+                int c = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    int x = 5;
+                    int y = x / c;
+                    Console.WriteLine($"Результат: {y}");
+                    Console.WriteLine("Если хотите повторить деление на другое число, то введите любое число кроме \"0\"");
+                    toExit = Convert.ToInt32(Console.ReadLine());
+                    Log.Fatal($"Все без фатальных ошибок c = {c}");
+                    Log.Error($"Все без ошибок c = {c}");
+                    Log.Warning($"Все без предупреждений c = {c}");
+                    Log.Info($"Просто информация c = {c}");
+                    Log.Info($"Просто информация со значениями c = {c}",x,y,c,toExit);
+                    Log.Debug($"Просто Debug c = {c}");
+                    Log.DebugFormat($"Просто Debug со значениями c = {c}", x, y, c, toExit);
+                    Dictionary<object, object> ForTest = new Dictionary<object, object>();
+                    ForTest.Add("x", x);
+                    ForTest.Add("y", y);
+                    ForTest.Add("c", c);
+                    ForTest.Add("Состояние Выхода", toExit);
+                    Log.SystemInfo($"Просто SysInfo с объектами c = {c}", ForTest);
+                    Log.SystemInfo("Просто SysInfo без объектов");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка деления!");
+                    Log.ErrorUnique($"(1)Ошибка уникальная = {c}", ex);
+                    Log.Fatal($"Фатальная ошибка = {c}",ex);
+                    Log.Error($"Ошибка = {c}",ex);
+                    Log.Error(ex);
+                    Log.ErrorUnique($"(2)Ошибка уникальная = {c}", ex);
+                    Log.WarningUnique($"Предупреждение = {c}");
+                    Log.Warning($"Предупреждение = {c}");
+                    Log.WarningUnique($"Предупреждение = {c}");
+                    Log.Info($"Просто информация с ошибкой c = {c}",ex);
+                    Log.Debug($"Просто Debug с ошибкой c = {c}");
+                    /*Console.WriteLine($"Исключение: {ex.Message}");
+                    Console.WriteLine($"Метод: {ex.TargetSite}");
+                    Console.WriteLine($"Трассировка стека: {ex.StackTrace}");*/
+                    Console.WriteLine("Если хотите повторить деление на другое число, то введите любое число кроме \"0\"");
+                    toExit = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
