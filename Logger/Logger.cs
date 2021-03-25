43b643d182252f6e1127a7c635430c33c92d89a2
Xom94o4ek <irek199707@gmail.com>
2021-03-25 18:48:56 +0300

    Правки приватности

--- Logger/Logger.cs
+++ Logger/Logger.cs
@@ -7,19 +7,18 @@ namespace Logger
 {
     class Logger : ILog
     {
-        public DateTime now;
         Dictionary<int, string> TodayErrors = new Dictionary<int, string>();
         int ErrorCounter = 0;
         Dictionary<int, string> TodayWarnings = new Dictionary<int, string>();
         int WarningCounter = 0;
-        string FatalFile = "Fatal.txt";
-        string ErrorFile = "Error.txt";
-        string ErrorUniqFile = "UError.txt";
-        string WarningFile = "Warning.txt";
-        string WarningUniqFile = "UWarning.txt";
-        string InfoFile = "Info.txt";
-        string DebugFile = "Debug.txt";
-        string SystemInfoFile = "SInfo.txt";
+        const string FatalFile = "Fatal.txt";
+        const string ErrorFile = "Error.txt";
+        const string ErrorUniqFile = "UError.txt";
+        const string WarningFile = "Warning.txt";
+        const string WarningUniqFile = "UWarning.txt";
+        const string InfoFile = "Info.txt";
+        const string DebugFile = "Debug.txt";
+        const string SystemInfoFile = "SInfo.txt";
         public void Fatal(string message)
         {
             WriteToFile(FatalFile, TextFormating("FATAL", message));
@@ -49,9 +48,6 @@ namespace Logger
         {
             if (!TodayErrors.ContainsValue($"{e.Message} - {e.StackTrace}"))
             {
-                //Error(message, e);
-                //ErrorCounter++;
-                //TodayErrors.Add(TodayErrors.Count, $"{e.Message} - {e.StackTrace}");
                 WriteToFile(ErrorUniqFile, TextFormating("UERROR", message, e));
             }
             Error(message, e);
@@ -72,9 +68,6 @@ namespace Logger
         {
             if (!TodayWarnings.ContainsValue(message))
             {
-                //Error(message, e);
-                //WarningCounter++;
-                //TodayWarnings.Add(WarningCounter, message);
                 WriteToFile(WarningUniqFile, TextFormating("UWARNING", message));
             }
             Warning(message);
@@ -111,29 +104,29 @@ namespace Logger
             }
             else { WriteToFile(SystemInfoFile, TextFormating("SYSINFO", message)); }
         }
-        public void WriteToFile(string FileName, string message)
+        private void WriteToFile(string FileName, string message)
         {
-            now = DateTime.Now;
+            DateTime now = DateTime.Now;
             string path = @String.Format("Log/{0}", now.ToString("yyyy-MM-dd"));
             DirectoryInfo dirInfo = new DirectoryInfo(path);
             if (!dirInfo.Exists) { dirInfo.Create(); TodayErrors.Clear(); ErrorCounter = 0; TodayWarnings.Clear(); WarningCounter = 0; }
             File.AppendAllText(@String.Format("{0}/{1}", path, FileName), $"{now.ToString("dd.MM.yyyy")} {now.ToString("HH:mm:ss")} {message}\n");
         }
-        public string TextFormating(string nametype, string message)
+        private string TextFormating(string nametype, string message)
         {
             return $"({nametype}): {message}";
         }
-        public string TextFormating(string nametype, string message, Exception e)
+        private string TextFormating(string nametype, string message, Exception e)
         {
             string ExcInfo = $"\n\t\t\t\tИсключение: {e.Message}\n\t\t\t\tМетод: {e.TargetSite}\n\t\t\t\tТрассировка стека: {e.StackTrace}";
             return $"({nametype}): {message}{ExcInfo}";
         }
-        public string TextFormating(string nametype, Exception e)
+        private string TextFormating(string nametype, Exception e)
         {
             string ExcInfo = $"Исключение: {e.Message}\n\t\t\t\tМетод: {e.TargetSite}\n\t\t\t\tТрассировка стека: {e.StackTrace}";
             return $"({nametype}): {ExcInfo}";
         }
-        public string TextFormating(string nametype, string message, params object[] args)
+        private string TextFormating(string nametype, string message, params object[] args)
         {
             string AllArgs = "";
             for (int i = 0; i < args.Length; i++)
@@ -142,7 +135,7 @@ namespace Logger
             }
             return $"({nametype}): {message}{AllArgs}";
         }
-        public string TextFormating(string nametype, string message, Dictionary<object, object> properties)
+        private string TextFormating(string nametype, string message, Dictionary<object, object> properties)
         {
             string AllArgs = "";
             foreach (KeyValuePair<object, object> keyValue in properties)
